52168e56804ae7df5e1ecfd46a66f0674e19b51b
mfikriam <mfikriam.pro@gmail.com>
2026-01-19 14:06:45 +0800

    Fix prisma service bug

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup() {
-    return { msg: 'I have signed up' };
+  async signup(dto: AuthDto) {
+    // Generate the password hash
+    const hash = await argon.hash(dto.password);
+
+    // Save the new user in the db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+
+    // Return the saved user
+    return user;
   }
 
   signin() {
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from 'src/generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/prisma/client';
+import { Pool } from 'pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    const adapter = new PrismaPg({ url: process.env.DATABASE_URL });
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    const url = process.env.DATABASE_URL;
+    if (!url) {
+      throw new Error('DATABASE_URL is not defined');
+    }
+
+    // 1. Create the pg Pool
+    const pool = new Pool({ connectionString: url });
+
+    // 2. Initialize the adapter with the pool
+    const adapter = new PrismaPg(pool);
+
+    // 3. Pass the adapter to the super constructor
     super({ adapter });
+  }
+
+  async onModuleInit() {
+    // Explicitly connect when the module initializes
+    await this.$connect();
   }
 }
