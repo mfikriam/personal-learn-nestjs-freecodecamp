9e94e35ea3a92304248e4794e417fa0ccdfb3a09
mfikriam <mfikriam.pro@gmail.com>
2026-01-19 12:00:23 +0800

    Add prima module

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,6 +6,7 @@ import { PrismaClient } from '../generated/prisma/client';
 export class PrismaService extends PrismaClient {
   constructor() {
     const adapter = new PrismaPg({ url: process.env.DATABASE_URL });
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     super({ adapter });
   }
 }
